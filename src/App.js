d2048eb386f4c2ebbc18d04b1628f43c98bba16f
Menmery <2053186460@qq.com>
2025-11-10 17:17:06 +0800

    JSX-列表渲染

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+const list = [
+  { id: 1, name: 'vue' },
+  { id: 2, name: 'react' },
+  { id: 3, name: 'angular' }
+]
 
 function App() {
   return (
     <div className="App">
-      react-basic
+      <ul>
+        {list.map(item => {
+          return <li key={item.id}>{item.name}</li>
+        })}
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+// 项目运行入口
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// 项目根组件
 import App from './App'
 
+// 挂载和渲染根组件
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
