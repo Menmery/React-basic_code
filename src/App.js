f3687f29d93cb26a236053882b86953c2b5fc864
Menmery <2053186460@qq.com>
2025-11-10 17:52:08 +0800

    状态变量

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-const Button = () => {
-  return <button>Click me</button>
-}
+import { useState } from "react"
 
 function App() {
+  // 1.调用useState添加一个状态变量
+  // count 就是状态变量
+  // setCount 就是更新状态变量的函数
+  const [count, setCount] = useState(0)
+
+  // 2.事件回调
+  const changeCount = () => {
+    setCount(count + 1)
+  }
+
   return (
     <div className="App">
-      <Button></Button>
+      <button onClick={changeCount}>{count}</button>
     </div>
   );
 }
