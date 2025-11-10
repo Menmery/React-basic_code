3187ece2d6c87be8743538c0459c422f59446f80
Menmery <2053186460@qq.com>
2025-11-10 17:43:18 +0800

    使用组件

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-const type = 2
-
-const getArticle = () => {
-  if (type === 1) {
-    return <div>无图模式模版</div>
-  } else if (type === 2) {
-    return <div>有图模式模版</div>
-  }
+const Button = () => {
+  return <button>Click me</button>
 }
 
 function App() {
   return (
     <div className="App">
-      {getArticle(type)}
+      <Button></Button>
     </div>
   );
 }
