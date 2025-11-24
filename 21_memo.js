88efa01094437e77f0c30d48062ce6d705a5e5c5
Menmery <2053186460@qq.com>
2025-11-24 09:19:57 +0800

    1

--- 21_memo.js
+++ 21_memo.js
@@ -4,6 +4,7 @@ import { memo, useState } from 'react'
 
 // memo
 // 作用：允许组件在props没有改变的情况下跳过重新渲染
+// 对于props的比较，进行的是‘浅比较’，底层使用 Object.is 进行比较，针对于对象数据类型，只会对比俩次的引用是否相等，如果不相等就会重新渲染，React并不关心对象中的具体属性
 const MemoSon = memo(function Son() {
   console.log('子组件被重新渲染了')
   return <div>this is span</div>
